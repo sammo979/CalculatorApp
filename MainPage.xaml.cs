4f27fcdb8e88b61086a4d3ffc46815ca37ab454a
Samantha Tredway <sam.tredway@trojans.dsu.edu>
2023-03-22 16:12:24 -0500

    add buttons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,88 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void ZeroClicked(object sender, EventArgs e)
+    {
+        CurrentCalculation.Text += "0"; 
+    }
+    private void OneClicked(object sender, EventArgs e)
 	{
-		count++;
+        CurrentCalculation.Text += "1";
+    }
+    private void TwoClicked(object sender, EventArgs e)
+    {
+        CurrentCalculation.Text += "2";
+    }
+    private void ThreeClicked(object sender, EventArgs e)
+    {
+        CurrentCalculation.Text += "3";
+    }
+    private void FourClicked(object sender, EventArgs e)
+    {
+        CurrentCalculation.Text += "4";
+    }
+    private void FiveClicked(object sender, EventArgs e)
+    {
+        CurrentCalculation.Text += "5";
+    }
+    private void SixClicked(object sender, EventArgs e)
+    {
+        CurrentCalculation.Text += "6";
+    }
+    private void SevenClicked(object sender, EventArgs e)
+    {
+        CurrentCalculation.Text += "7";
+    }
+    private void EightClicked(object sender, EventArgs e)
+    {
+        CurrentCalculation.Text += "8";
+    }
+    private void NineClicked(object sender, EventArgs e)
+    {
+        CurrentCalculation.Text += "9";
+    }
+    private void ClearClicked(object sender, EventArgs e)
+    {
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    }
+    private void PosNegClicked(object sender, EventArgs e)
+    {
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    }
+    private void PercentageClicked(object sender, EventArgs e)
+    {
+
+    }
+    private void DivideClicked(object sender, EventArgs e)
+    {
+
+    }
+    private void MultiplyClicked(object sender, EventArgs e)
+    {
+
+    }
+    private void PlusClicked(object sender, EventArgs e)
+    {
+
+    }
+    private void MinusClicked(object sender, EventArgs e)
+    {
+
+    }
+    private void EqualClicked(object sender, EventArgs e)
+    {
+
+    }
+    private void DecimalClicked(object sender, EventArgs e)
+    {
+
+    }
+   
 }
 
